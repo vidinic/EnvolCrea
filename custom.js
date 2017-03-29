620d953ddf35782773e9b6ddc56c1e847e0b61bd
pauline <vidinicpauline@gmail.com>
2017-03-29 16:31:52 +0200

    scroll section

--- custom.js
+++ custom.js
@@ -1,5 +1,28 @@
+/* effet parallax */
+
 $(document).ready(function(){
     $('.parallax').parallax();
+});
+
+
+/* scroll de navigation */
+
+$(document).ready(function() {
+    $('#nav li a').click(function () {
+        $('#nav li a').removeClass('active');
+        $(this).addClass('active');
+        var id = $(this).attr('href');
+        scrollTo(id);
+        return false;
+    });
+
+    function scrollTo(target) {
+        if ($(target).length >= 1) {
+            height = $(target).offset().top;
+        }
+        $('html, body').animate({scrollTo: height}, 600);
+        return false;
+    }
 });
 
 
